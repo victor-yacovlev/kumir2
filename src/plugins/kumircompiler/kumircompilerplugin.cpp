816a994100856a7cf75cef9862a4ebb04965c52d
victor <victor@382dccb8-6cdf-11e0-9640-eb60159f4418>
2011-06-17 14:16:31 +0000

    git-svn-id: http://lpm.org.ru/svn/kumir2@125 382dccb8-6cdf-11e0-9640-eb60159f4418

--- src/plugins/kumircompiler/kumircompilerplugin.cpp
+++ src/plugins/kumircompiler/kumircompilerplugin.cpp
@@ -153,6 +153,7 @@ void KumirCompilerPlugin::start()
         std::cerr << "\tFILENAME.kum\tKumir program input file name" << std::endl;
         qApp->setProperty("returnCode", 127);
     }
+    qApp->quit();
 }
 
 void KumirCompilerPlugin::stop()
