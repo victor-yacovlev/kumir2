7198e432eb4ef42b0d62401b16790ecdb4d77e0c
victor <victor@382dccb8-6cdf-11e0-9640-eb60159f4418>
2011-10-08 11:32:44 +0000

    git-svn-id: http://lpm.org.ru/svn/kumir2@429 382dccb8-6cdf-11e0-9640-eb60159f4418

--- src/Fpc-Pascal.cmake
+++ src/Fpc-Pascal.cmake
@@ -1,8 +1,3 @@
-find_program(FPC_COMPILER fpc
-    NAMES fpc ppc386 ppcx64
-    PATHS /usr/bin /usr/local/bin
-)
-
 if(${CMAKE_SYSTEM_PROCESSOR} MATCHES "x86_64")
     set(FPC_TARGET x86_64)
 else()
@@ -15,7 +10,33 @@ if(WIN32)
 	set(FPC_SYSTEM_NAME win32)
 endif(WIN32)
 
-set(FPC_COMMON_FLAGS
+
+if(EXISTS ${CMAKE_SOURCE_DIR}/fpc/lib/fpc/2.4.4)
+  find_program(FPC_COMPILER fpc
+      NAMES ppc386 ppcx64
+      PATHS ${CMAKE_SOURCE_DIR}/fpc/lib/fpc/2.4.4
+  )
+  set(FPC_COMMON_FLAGS
     -T${FPC_SYSTEM_NAME}
     -d${FPC_TARGET}
-)
+    -Fu${CMAKE_SOURCE_DIR}/fpc/lib/fpc/2.4.4/units/${FPC_TARGET}-${FPC_SYSTEM_NAME}/rtl
+  )
+  
+else()
+
+  find_program(FPC_COMPILER fpc
+      NAMES fpc ppc386 ppcx64
+      PATHS /usr/bin /usr/local/bin
+  )
+  set(FPC_COMMON_FLAGS
+      -T${FPC_SYSTEM_NAME}
+      -d${FPC_TARGET}
+  )
+  
+endif()
+
+if(EXISTS ${FPC_COMPILER})
+  message("-- FreePascal compiler: ${FPC_COMPILER}")
+else()
+  message(FATAL_ERROR "FreePascal compiler (version >= 2.4.0) not found")
+endif()
