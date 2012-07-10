873151231ddf04b54b4bfba1f7e51c612cc21b29
Victor Yacovlev <v.yacovlev@gmail.com>
2012-07-10 18:40:37 +0400

    Created preliminary skeleton for Robot Actor

---
+++ src/actors/robot/robotmodule.cpp
@@ -0,0 +1,119 @@
+/*
+This file is generated, but you can safely change it
+until you run "gen_actor_source.py" with "--project" flag.
+
+Generated file is just a skeleton for module contents. 
+You should change it corresponding to functionality.
+*/
+
+#include <QtCore>
+#include <QtGui>
+#include "robotmodule.h"
+
+namespace ActorRobot {
+
+RobotModule::RobotModule(ExtensionSystem::KPlugin * parent)
+	: RobotModuleBase(parent)
+{
+	/* TODO 
+	implement class Constructor
+	*/
+}
+
+void RobotModule::reset()
+{
+	/* TODO
+	This method is called when actor resets its state before program starts.
+	*/
+}
+
+void RobotModule::setAnimationEnabled(bool enabled)
+{
+	/* TODO
+	Set internal flag for animation
+	*/
+}
+
+
+QWidget* RobotModule::mainWidget() const
+{
+	/* TODO
+	This method should return a pointer to main widget.
+	NOTE: This metod should NOT create main widget -- just return!
+	*/
+	return 0;
+}
+
+QWidget* RobotModule::pultWidget() const
+{
+	/* TODO
+	This method should return a pointer to pult widget.
+	NOTE: This metod should NOT create pult widget -- just return!
+	*/
+	return 0;
+}
+
+void RobotModule::runGoUp()
+{
+	/* TODO implement me */
+	return;
+}
+
+
+void RobotModule::runGoDown()
+{
+	/* TODO implement me */
+	return;
+}
+
+
+void RobotModule::runGoLeft()
+{
+	/* TODO implement me */
+	return;
+}
+
+
+void RobotModule::runGoRight()
+{
+	/* TODO implement me */
+	return;
+}
+
+
+void RobotModule::runDoPaint()
+{
+	/* TODO implement me */
+	return;
+}
+
+
+bool RobotModule::runIsWallAtTop()
+{
+	/* TODO implement me */
+	return false;
+}
+
+
+bool RobotModule::runIsWallAtBottom()
+{
+	/* TODO implement me */
+	return false;
+}
+
+
+bool RobotModule::runIsWallAtLeft()
+{
+	/* TODO implement me */
+	return false;
+}
+
+
+bool RobotModule::runIsWallAtRight()
+{
+	/* TODO implement me */
+	return false;
+}
+
+	
+} // $namespace
---
+++ src/actors/robot/robotmodule.h
@@ -0,0 +1,51 @@
+/*
+This file is generated, but you can safely change it
+until you run "gen_actor_source.py" with "--project" flag.
+
+Generated file is just a skeleton for module contents. 
+You should change it corresponding to functionality.
+*/
+
+#ifndef ROBOTMODULE_H
+#define ROBOTMODULE_H
+
+#include <QtCore>
+#include <QtGui>
+#include "extensionsystem/kplugin.h"
+#include "robotmodulebase.h"
+
+namespace ActorRobot {
+
+class RobotModule
+	: public RobotModuleBase
+{
+	Q_OBJECT
+public:
+	// Constructor
+	RobotModule(ExtensionSystem::KPlugin * parent);
+public slots:
+	// Reset actor state before program starts
+	void reset();
+	// Set animation flag
+	void setAnimationEnabled(bool enabled);
+	// Actor methods
+	void runGoUp();
+	void runGoDown();
+	void runGoLeft();
+	void runGoRight();
+	void runDoPaint();
+	bool runIsWallAtTop();
+	bool runIsWallAtBottom();
+	bool runIsWallAtLeft();
+	bool runIsWallAtRight();
+
+public:
+	// GUI access methods
+	QWidget* mainWidget() const;
+	QWidget* pultWidget() const;
+
+}; // RobotModule
+	
+} // ActorRobot
+
+#endif // ROBOTMODULE_H
