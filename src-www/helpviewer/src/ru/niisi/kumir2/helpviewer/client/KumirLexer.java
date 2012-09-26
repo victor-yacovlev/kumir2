29aeace2eee2f7822dc6b1215e4ce56ac7ed0915
Victor Yacovlev <victor@mint>
2012-09-26 20:13:49 +0400

    GIT ignore list for eclipse/idea project files

---
+++ src-www/helpviewer/src/ru/niisi/kumir2/helpviewer/client/KumirLexer.java
@@ -0,0 +1,11 @@
+package ru.niisi.kumir2.helpviewer.client;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: victor
+ * Date: 9/26/12
+ * Time: 1:46 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class KumirLexer {
+}
