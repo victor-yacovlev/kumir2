6db8967430e76f2e8c97b22b6fe9e78f61ac36a0
Victor Yacovlev <v.yacovlev@gmail.com>
2013-02-07 20:48:50 +0400

    GUI: implemented autocomplete

---
+++ src/plugins/editor/suggestionswindow.cpp
@@ -0,0 +1,253 @@
+#include "suggestionswindow.h"
+#include "ui_suggestionswindow.h"
+#include "settingspage.h"
+
+namespace Editor {
+
+SuggestionsWindow::SuggestionsWindow(QWidget *parent) :
+    QWidget(parent),
+    ui(new Ui::SuggestionsWindow)
+{
+    ui->setupUi(this);
+    ui->alist->installEventFilter(this);
+    connect(ui->alist, SIGNAL(currentRowChanged(int)),
+            this, SLOT(handleCurrentItemChanged(int)));
+    connect(ui->alist, SIGNAL(itemActivated(QListWidgetItem*)),
+            this, SLOT(handleItemActivated(QListWidgetItem*)));
+    connect(ui->btnAccept, SIGNAL(clicked()), this, SLOT(acceptItem()));
+    connect(ui->btnClose, SIGNAL(clicked()), this, SLOT(hide()));
+}
+
+void SuggestionsWindow::updateSettings(const QSettings *settings)
+{
+    createIcons(settings);
+    QFont f;
+    f.setFamily(settings->value(
+                    SettingsPage::KeyFontName,
+                    SettingsPage::defaultFontFamily()
+                    ).toString());
+    f.setPointSize(settings->value(
+                       SettingsPage::KeyFontSize,
+                       SettingsPage::defaultFontSize
+                       ).toInt());
+    ui->alist->setFont(f);
+}
+
+void SuggestionsWindow::createIcons(const QSettings *settings)
+{
+    QPoint points[4] = { QPoint(4,0), QPoint(0,4), QPoint(4,8), QPoint(8,4) };
+    QImage local(8,8,QImage::Format_ARGB32);
+    local.fill(0);
+    QPainter pl(&local);
+    pl.setRenderHint(QPainter::Antialiasing, true);
+    pl.setPen(QPen(QColor("black")));
+    pl.setBrush(QColor(settings->value(
+                           SettingsPage::KeyColorType,
+                           SettingsPage::DefaultColorType
+                           ).toString()));
+    pl.drawEllipse(QRect(0,0,8,8));
+
+    icon_local = QIcon(QPixmap::fromImage(local));
+
+    QImage global(8,8,QImage::Format_ARGB32);
+    global.fill(0);
+    QPainter pg(&global);
+    pg.setRenderHint(QPainter::Antialiasing, true);
+    pg.setPen(QPen(QColor("black")));
+    pg.setBrush(QColor(settings->value(
+                           SettingsPage::KeyColorType,
+                           SettingsPage::DefaultColorType
+                           ).toString()));
+    pg.drawPolygon(points, 4);
+
+    icon_global = QIcon(QPixmap::fromImage(global));
+
+    QImage algorithm(8,8,QImage::Format_ARGB32);
+    algorithm.fill(0);
+    QPainter pa(&algorithm);
+    pa.setRenderHint(QPainter::Antialiasing, true);
+    pa.setPen(QPen(QColor("black")));
+    pa.setBrush(QColor(settings->value(
+                           SettingsPage::KeyColorAlg,
+                           SettingsPage::DefaultColorAlg
+                           ).toString()));
+    pa.drawPolygon(points, 4);
+
+    icon_algorithm = QIcon(QPixmap::fromImage(algorithm));
+
+    QImage module(8,8,QImage::Format_ARGB32);
+    module.fill(0);
+    QPainter pm(&module);
+    pm.setRenderHint(QPainter::Antialiasing, true);
+    pm.setPen(QPen(QColor("black")));
+    pm.setBrush(QColor(settings->value(
+                           SettingsPage::KeyColorMod,
+                           SettingsPage::DefaultColorMod
+                           ).toString()));
+    pm.drawRect(QRect(1,1,6,6));
+
+    icon_module = QIcon(QPixmap::fromImage(module));
+
+    QImage kumfile(8,8,QImage::Format_ARGB32);
+    kumfile.fill(0);
+    QPainter pf(&kumfile);
+    pf.setRenderHint(QPainter::Antialiasing, true);
+    pf.setPen(QPen(QColor("black")));
+    pf.setBrush(QColor(settings->value(
+                           SettingsPage::KeyColorLiteral,
+                           SettingsPage::DefaultColorLiteral
+                           ).toString()));
+    pf.drawRect(QRect(1,1,6,6));
+
+    icon_kumfile = QIcon(QPixmap::fromImage(kumfile));
+
+    QImage keyword(8,8,QImage::Format_ARGB32);
+    keyword.fill(0);
+    QPainter pk(&keyword);
+    pk.setRenderHint(QPainter::Antialiasing, true);
+    pk.setPen(QPen(QColor("black")));
+    pk.setBrush(QColor(settings->value(
+                           SettingsPage::KeyColorKw,
+                           SettingsPage::DefaultColorKw
+                           ).toString()));
+    pk.drawRect(QRect(1,1,6,6));
+
+    icon_keyword = QIcon(QPixmap::fromImage(keyword));
+
+    QImage other(8,8,QImage::Format_ARGB32);
+    other.fill(0);
+    icon_other = QIcon(QPixmap::fromImage(other));
+
+}
+
+void SuggestionsWindow::handleCurrentItemChanged(int currentRow)
+{
+    if (currentRow>=0 && currentRow<l_suggestions.size()) {
+        ui->descriptionView->setText(l_suggestions.at(currentRow).description);
+    }
+    else {
+        ui->descriptionView->clear();
+    }
+}
+
+void SuggestionsWindow::handleItemActivated(QListWidgetItem *)
+{
+    acceptItem();
+}
+
+void SuggestionsWindow::acceptItem()
+{
+    int row = ui->alist->currentRow();
+    if (row>=0 && row<ui->alist->count()) {
+        const QString value = l_suggestions.at(row).value;
+        emit acceptedSuggestion(value);
+    }
+    hide();
+}
+
+void SuggestionsWindow::init(
+        const QString &,
+        const QList<Shared::Suggestion> &suggestions)
+{
+    b_keyPressed = false;
+    ui->alist->clear();
+    ui->descriptionView->clear();
+    for (int index = 0; index<suggestions.size(); index++) {
+        const Shared::Suggestion & s = suggestions.at(index);
+        QListWidgetItem * item = new QListWidgetItem(ui->alist);
+        item->setText(s.value);
+        if (s.kind==Shared::Suggestion::Kind::Local) {
+            item->setIcon(icon_local);
+        }
+        else if (s.kind==Shared::Suggestion::Kind::Global) {
+            item->setIcon(icon_global);
+        }
+        else if (s.kind==Shared::Suggestion::Kind::Algorithm) {
+            item->setIcon(icon_algorithm);
+        }
+        else if (s.kind==Shared::Suggestion::Kind::BuiltinModule) {
+            item->setIcon(icon_module);
+        }
+        else if (s.kind==Shared::Suggestion::Kind::KumirModule) {
+            item->setIcon(icon_kumfile);
+        }
+        else if (s.kind==Shared::Suggestion::Kind::SecondaryKeyword) {
+            item->setIcon(icon_keyword);
+        }
+        else {
+            item->setIcon(icon_other);
+        }
+    }
+    if (l_suggestions.size()==0) {
+        ui->descriptionView->setText(tr("No suggestions"));
+    }
+    l_suggestions = suggestions;
+}
+
+void SuggestionsWindow::focusInEvent(QFocusEvent *event)
+{
+    ui->alist->setFocus(event->reason());
+    event->accept();
+    if (ui->alist->count()>0) {
+        ui->alist->setCurrentRow(0);
+    }
+}
+
+bool SuggestionsWindow::eventFilter(QObject *obj, QEvent *event)
+{
+    if (obj==ui->alist && event->type()==QEvent::KeyPress) {
+        QKeyEvent * e = static_cast<QKeyEvent*>(event);
+        if (e->key()==Qt::Key_Tab || e->key()==Qt::Key_Escape) {
+            hide();
+            return true;
+        }
+    }
+    return false;
+}
+
+void SuggestionsWindow::keyPressEvent(QKeyEvent *event)
+{
+    b_keyPressed = true;
+    if (event->key()==Qt::Key_Tab || event->key()==Qt::Key_Escape) {
+        hide();
+        event->accept();
+    }
+    else if (event->key()==Qt::Key_Enter || event->key()==Qt::Key_Return) {
+        hide();
+        event->accept();
+    }
+    else {
+        event->ignore();
+    }
+}
+
+void SuggestionsWindow::keyReleaseEvent(QKeyEvent *event)
+{
+    if (b_keyPressed || event->key()==Qt::Key_Escape) {
+        b_keyPressed = false;
+        if (event->key()==Qt::Key_Tab || event->key()==Qt::Key_Escape) {
+            hide();
+        }
+        else if (event->key()==Qt::Key_Enter || event->key()==Qt::Key_Return) {
+            hide();
+        }
+        event->accept();
+    }
+    else {
+        event->ignore();
+    }
+}
+
+void SuggestionsWindow::hideEvent(QHideEvent *event)
+{
+    b_keyPressed = false;
+    QWidget::hideEvent(event);
+    parentWidget()->setFocus();
+}
+
+SuggestionsWindow::~SuggestionsWindow()
+{
+    delete ui;
+}
+
+} // namespace Editor
---
+++ src/plugins/editor/suggestionswindow.h
@@ -0,0 +1,51 @@
+#ifndef EDITOR_SUGGESTIONSWINDOW_H
+#define EDITOR_SUGGESTIONSWINDOW_H
+
+#include <QtGui>
+
+#include "interfaces/analizerinterface.h"
+
+namespace Editor {
+
+namespace Ui {
+class SuggestionsWindow;
+}
+
+class SuggestionsWindow : public QWidget
+{
+    Q_OBJECT
+    
+public:
+    explicit SuggestionsWindow(QWidget *parent = 0);
+    void init(const QString & before, const QList<Shared::Suggestion> & suggestions);
+    ~SuggestionsWindow();
+    void updateSettings(const QSettings * settings);
+signals:
+    void acceptedSuggestion(const QString & text);
+protected:
+    void focusInEvent(QFocusEvent * event);
+    void keyPressEvent(QKeyEvent * event);
+    void keyReleaseEvent(QKeyEvent *event);
+    void hideEvent(QHideEvent * event);
+    bool eventFilter(QObject * obj, QEvent * event);
+protected slots:
+    void handleCurrentItemChanged(int currentRow);
+    void handleItemActivated(QListWidgetItem * item);
+    void acceptItem();
+private:
+    void createIcons(const QSettings * settings);
+    Ui::SuggestionsWindow *ui;
+    QList<Shared::Suggestion> l_suggestions;
+    bool b_keyPressed = false;
+    QIcon icon_local;
+    QIcon icon_global;
+    QIcon icon_algorithm;
+    QIcon icon_keyword;
+    QIcon icon_module;
+    QIcon icon_kumfile;
+    QIcon icon_other;
+};
+
+
+} // namespace Editor
+#endif // EDITOR_SUGGESTIONSWINDOW_H
