59842963881b69f9b2aa4035190c90310ff9e3b0
Victor Yacovlev <v.yacovlev@gmail.com>
2013-04-23 18:45:35 +0400

    GUI: Win32: Fixed crash on drawing settings groupboxes

--- src/shared/widgets/groupbox.cpp
+++ src/shared/widgets/groupbox.cpp
@@ -29,16 +29,43 @@ void GroupBox::mouseReleaseEvent(QMouseEvent *event)
     QGroupBox::mouseReleaseEvent(event);
 }
 
+
+#ifdef Q_OS_WIN32
+void GroupBox::paintEvent(QPaintEvent *event)
+{
+    QPainter p(this);
+    p.setRenderHint(QPainter::Antialiasing, false);
+    p.setRenderHint(QPainter::TextAntialiasing, true);
+    p.setPen(QPen(QColor("lightgray")));
+    QRect lineRect(2, 2, width()-4, height()-4);
+    p.drawRect(lineRect);
+    p.setBrush(QColor("white"));
+    p.setPen(QPen(QBrush(QColor("black")), 1.0));
+    p.drawRect(6, 5, 8, 8);
+    p.drawLine(6, 9, 13, 9);
+    if (isCollapsed()) {
+        p.drawLine(10, 5, 10, 13);
+    }
+    QFont f = font();
+    f.setBold(true);
+    p.setFont(f);
+    int textHeight = QFontMetrics(f).height();
+    p.drawText(24,textHeight,title());
+    event->accept();
+}
+#else
 void GroupBox::paintEvent(QPaintEvent *event)
 {
     QPainter p(this);
     QRect lineRect(2, 2, width()-4, height()-4);
     QStyleOptionFrame frameOpt;
+    frameOpt.initFrom(this);
     frameOpt.rect = lineRect;
     p.save();
     style()->drawPrimitive(QStyle::PE_FrameGroupBox, &frameOpt, &p, this);
     p.restore();
     QStyleOption btnOpt;
+    btnOpt.initFrom(this);
     btnOpt.state = QStyle::State_Children | (!isCollapsed()? QStyle::State_Open : QStyle::State_None);
     btnOpt.rect = QRect(4,4,16,16);
     p.save();
@@ -51,6 +78,7 @@ void GroupBox::paintEvent(QPaintEvent *event)
     p.drawText(24,textHeight,title());
     event->accept();
 }
+#endif
 
 void GroupBox::setCollapsed(bool collapse)
 {
