28a1d440b2efd42a9bf0b57dfe044076f36cdae7
Victor Yacovlev <v.yacovlev@gmail.com>
2013-05-28 14:13:01 +0400

    KumFile: Implemented RSA-key signing for hidden text part

--- src/shared/dataformats/kumfile.h
+++ src/shared/dataformats/kumfile.h
@@ -18,12 +18,37 @@ struct Data
     QString hiddenText;
     bool hasHiddenText;
     QString sourceEncoding;
-
+    QByteArray hiddenTextSignature;
 };
 
 KUMFILE_EXPORT QString toString(const Data & data);
 KUMFILE_EXPORT Data fromString(const QString &s);
 KUMFILE_EXPORT QString readRawDataAsString(QByteArray rawData, const QString & sourceEncoding);
+
+KUMFILE_EXPORT bool hasCryptographicRoutines();
+KUMFILE_EXPORT void generateKeyPair(
+        const QString & passPhrase,
+        QString& privateKey,
+        QString& publicKey
+        );
+
+KUMFILE_EXPORT void signHiddenText(
+        Data& data,
+        const QString & privateKey,
+        const QString & passPhrase
+        );
+
+enum VerifyResult {
+    SignatureMatch,
+    SignatureMismatch,
+    NoSignature,
+    CryptographyNotSupported
+};
+
+KUMFILE_EXPORT VerifyResult verifyHiddenText(
+        const Data& data,
+        const QString& publicKey
+        );
 
 } // namespace KumFile
 
