81e07857b9472332b4643f013e06cd42cdf45afb
Victor Yacovlev <v.yacovlev@gmail.com>
2013-08-28 14:30:05 +0400

    Performance fix on load extra fonts for DocBookViewer

--- src/shared/docbookviewer/contentview.cpp
+++ src/shared/docbookviewer/contentview.cpp
@@ -33,19 +33,19 @@ ContentView::ContentView(QWidget *parent)
             this, SLOT(clearLastAnchorUrl()));
     ignoreClearAnchorUrl_ = false;
 
-    if (!ExtraFontsLoaded_) {
-        const QString resourcesRoot = QCoreApplication::instance()->property("sharePath").toString();
-        const QDir fontsDir = QDir(resourcesRoot + "/docbookviewer");
-        const QStringList ttfFiles = fontsDir.entryList(QStringList() << "*.ttf" << "*.otf");
-        foreach (const QString & fileName, ttfFiles) {
-            const QString filePath = fontsDir.absoluteFilePath(fileName);
-            int id = QFontDatabase::addApplicationFont(filePath);
-            if (id == -1) {
-                qWarning() << "Can't load font " << filePath;
-            }
-        }
-        ExtraFontsLoaded_ = true;
-    }
+//    if (!ExtraFontsLoaded_) {
+//        const QString resourcesRoot = QCoreApplication::instance()->property("sharePath").toString();
+//        const QDir fontsDir = QDir(resourcesRoot + "/docbookviewer");
+//        const QStringList ttfFiles = fontsDir.entryList(QStringList() << "*.ttf" << "*.otf");
+//        foreach (const QString & fileName, ttfFiles) {
+//            const QString filePath = fontsDir.absoluteFilePath(fileName);
+//            int id = QFontDatabase::addApplicationFont(filePath);
+//            if (id == -1) {
+//                qWarning() << "Can't load font " << filePath;
+//            }
+//        }
+//        ExtraFontsLoaded_ = true;
+//    }
 
 }
 
