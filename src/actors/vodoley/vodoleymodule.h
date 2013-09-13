7e55e87805da4efaa29522152e97af0fe742ab53
Denis Khachko <mordol@lpm.org.ru>
2013-09-13 16:41:39 +0400

    Vodoley module n course manager Web debug.

--- src/actors/vodoley/vodoleymodule.h
+++ src/actors/vodoley/vodoleymodule.h
@@ -6,11 +6,11 @@ Generated file is just a skeleton for module contents.
 You should change it corresponding to functionality.
 */
 
-#ifndef DRAWMODULE_H
-#define DRAWMODULE_H
+#ifndef VODOLEYMODULE_H
+#define VODOLEYMODULE_H
 
 // Base class include
-#include "drawmodulebase.h"
+#include "vodoleymodulebase.h"
 
 // Kumir includes
 #include "extensionsystem/kplugin.h"
@@ -19,15 +19,15 @@ You should change it corresponding to functionality.
 #include <QtCore>
 #include <QtGui>
 
-namespace ActorDraw {
+namespace ActorVodoley {
 
 
-class DrawModule
-    : public DrawModuleBase
+class VodoleyModule
+    : public VodoleyModuleBase
 {
     Q_OBJECT
 public /* methods */:
-    DrawModule(ExtensionSystem::KPlugin * parent);
+    VodoleyModule(ExtensionSystem::KPlugin * parent);
     static QList<ExtensionSystem::CommandLineParameter> acceptableCommandLineParameters();
     QWidget* mainWidget() const;
     QWidget* pultWidget() const;
@@ -37,14 +37,19 @@ public slots:
     void reloadSettings(ExtensionSystem::SettingsPtr settings, const QStringList & keys);
     void reset();
     void setAnimationEnabled(bool enabled);
-    void runSetupPen();
-    void runReleasePen();
-    void runSetPenColor(const Color& color);
-    void runMoveTo(const qreal x, const qreal y);
-    void runMoveBy(const qreal dX, const qreal dY);
-    void runAddCaption(const qreal width, const QString& text);
-    Color runOperatorINPUT(const QString& x, bool& ok);
-    QString runOperatorOUTPUT(const Color& x);
+    void runFillA();
+    void runFillB();
+    void runFillC();
+    void runEmptyA();
+    void runEmptyB();
+    void runEmptyC();
+    void runFromAToB();
+    void runFromAToC();
+    void runFromBToA();
+    void runFromBToC();
+    void runFromCToB();
+    void runFromCToA();
+    bool runTaskComplited();
 
 
 
@@ -58,6 +63,6 @@ public slots:
 };
         
 
-} // namespace ActorDraw
+} // namespace ActorVodoley
 
-#endif // DRAWMODULE_H
+#endif // VODOLEYMODULE_H
