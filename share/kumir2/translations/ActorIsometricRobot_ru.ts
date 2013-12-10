2efcb9d37d062b92e945194d368f2aacb30c2530
Victor Yacovlev <v.yacovlev@gmail.com>
2013-12-10 19:55:10 +0400

    Actor's fixed

---
+++ share/kumir2/translations/ActorIsometricRobot_ru.ts
@@ -0,0 +1,74 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="ru_RU">
+<context>
+    <name>Robot25DWindow</name>
+    <message>
+        <location filename="../../../src/actors/isometricrobot/robot25dwindow.ui" line="14"/>
+        <location filename="../../../src/actors/isometricrobot/robot25dwindow.cpp" line="25"/>
+        <source>Isometric Robot</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message utf8="true">
+        <location filename="../../../src/actors/isometricrobot/robot25dwindow.ui" line="23"/>
+        <source>Загрузить...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message utf8="true">
+        <location filename="../../../src/actors/isometricrobot/robot25dwindow.ui" line="26"/>
+        <source>Загрузить</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message utf8="true">
+        <location filename="../../../src/actors/isometricrobot/robot25dwindow.ui" line="29"/>
+        <source>Загрузить обстановку или набор из файла</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message utf8="true">
+        <location filename="../../../src/actors/isometricrobot/robot25dwindow.ui" line="34"/>
+        <source>Следующая обстановка</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message utf8="true">
+        <location filename="../../../src/actors/isometricrobot/robot25dwindow.ui" line="37"/>
+        <source>Вперед</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message utf8="true">
+        <location filename="../../../src/actors/isometricrobot/robot25dwindow.ui" line="42"/>
+        <source>Предыдущая обстановка</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message utf8="true">
+        <location filename="../../../src/actors/isometricrobot/robot25dwindow.ui" line="45"/>
+        <source>Назад</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message utf8="true">
+        <location filename="../../../src/actors/isometricrobot/robot25dwindow.ui" line="50"/>
+        <source>Вернуть обстановку к начальной</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message utf8="true">
+        <location filename="../../../src/actors/isometricrobot/robot25dwindow.ui" line="53"/>
+        <source>Вернуть обстановку</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../src/actors/isometricrobot/robot25dwindow.cpp" line="172"/>
+        <location filename="../../../src/actors/isometricrobot/robot25dwindow.cpp" line="178"/>
+        <source>Can&apos;t load environment</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../src/actors/isometricrobot/robot25dwindow.cpp" line="187"/>
+        <source>Load environment</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../src/actors/isometricrobot/robot25dwindow.cpp" line="189"/>
+        <source>Environments</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
