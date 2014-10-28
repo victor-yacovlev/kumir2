5f53216acfcca80737958cb7b3c107173fff6247
Victor Yacovlev <v.yacovlev@gmail.com>
2014-10-28 14:47:00 +0300

    Changed required for #kumir2-1791

--- src/shared/widgets/dockwindowplace_container.cpp
+++ src/shared/widgets/dockwindowplace_container.cpp
@@ -42,6 +42,7 @@ void DockWindowPlaceContainer::activate(const QPoint &, const QSize & sz)
     place_->setCurrentIndex(tabIndex);
     place_->activate(sz);
     centralWidget_->setFocus();
+    notifyOnDocked();
 }
 
 void DockWindowPlaceContainer::deactivate()
