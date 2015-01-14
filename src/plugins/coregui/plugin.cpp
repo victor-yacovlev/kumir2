04f1b8fe407b94d644ff7ecf8c5798ebc5fd0aab
Victor Yacovlev <v.yacovlev@gmail.com>
2015-01-14 16:09:09 +0300

    Fixed #kumir2-1835

--- src/plugins/coregui/plugin.cpp
+++ src/plugins/coregui/plugin.cpp
@@ -832,6 +832,7 @@ void Plugin::start()
     if (!sessionsDisableFlag_ && showDialogOnStartup) {
         if (!showWorkspaceChooseDialog()) {
             ExtensionSystem::PluginManager::instance()->shutdown();
+            return;
         }
     }
     else {
