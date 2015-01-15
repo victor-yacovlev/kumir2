b495e8381278deeb15f3e64a00cd4d849b4f4fe9
Victor Yacovlev <v.yacovlev@gmail.com>
2015-01-15 23:33:48 +0300

    Updated interface to not conflict with python branch

--- src/shared/interfaces/analizer_instanceinterface.h
+++ src/shared/interfaces/analizer_instanceinterface.h
@@ -25,7 +25,9 @@ struct Error
     int line; // line number from 0
     int start; // position in line from 0
     int len; // error markup length
-    QString message; // error code
+    QString message; // error message
+    QByteArray msgid;  // internal message id
+    QByteArray origin;  // message origin name (PyLint, PyFlakes etc.)
 };
 
 typedef QVector<LexemType> LineProp;
