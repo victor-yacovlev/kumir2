54272012a6467b5c48a0a11d92f829aeb9838871
Victor Yacovlev <v.yacovlev@gmail.com>
2015-01-16 20:11:22 +0300

    Example course

---
+++ share/kumir2/courses/python/py/01-03.py
@@ -0,0 +1,50 @@
+﻿# Заданы два числа с именами a и b.
+#
+# Создайте имя d, в котором хранится сумма
+# чисел a и b, и имя f, в котором хранится
+# их произведение.
+
+a = 3
+b = 4.5
+
+
+# -*- kumir: begin hidden text -*-
+def __pre_test__():
+    import _kumir
+    _kumir.set_test_run_count(3)
+    _kumir.set_permanent_value("a_values", [1, 2, 3])
+    _kumir.set_permanent_value("b_values", [2.5, 3.5, 4.5])
+    
+    
+def __pre_run__():
+    import _kumir
+    a_values = _kumir.get_permanent_value("a_values")
+    b_values = _kumir.get_permanent_value("b_values")
+    assert len(a_values) == len(b_values)    
+    index = 3 - _kumir.get_test_runs_left()
+    a = a_values[index]
+    b = b_values[index]
+    _kumir.force_global_variable_value("a", a_values[index])
+    _kumir.force_global_variable_value("b", b_values[index])
+
+def __post_run__():    
+    result = 0
+    try:
+        if d == a + b:
+            result += 5
+        else:
+            result += 1
+    except NameError:
+        pass
+    try:
+        if f == a * b:
+            result += 5
+        else:
+            result += 1
+    except NameError:
+        pass
+    return result
+    
+    
+
+# -*- kumir: protected lines: 0 1 2 3 4 5 6 7 -*-
---
+++ share/kumir2/courses/python/py/01-05.py
@@ -0,0 +1,39 @@
+﻿# Дан список нечетной длины с именем s.
+#
+# 1. Сохраните в переменной head
+#    первый элемент списка
+# 2. Сохраните в переменной tail
+#    "хвост" списка, то есть
+#    все элементы, кроме первого
+# 3. Выведете на экран элемент списка,
+#    который находится ровно посередине
+
+s = [1, 2, 3, 4, 5]
+
+
+# -*- kumir: begin hidden text -*-
+
+def __pre_run__():
+    import _kumir
+    _kumir.force_global_variable_value("s", [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+
+def __post_run__():
+    result = 0
+    try:
+        if head==0:
+            result += 2
+    except:
+        pass
+    try:
+        if tail==[1, 2, 3, 4, 5, 6, 7, 8, 9, 10]:
+            result += 3
+    except:
+        pass
+    import _kumir
+    lines = _kumir.get_output_buffer().strip().split('\n')
+    last_line = lines[-1]
+    if last_line=="5":
+        result += 5
+    return result
+
+# -*- kumir: protected lines: 0 1 2 3 4 5 6 7 8 9 10  -*-
