5c5d7ba0a778af79ee753806e8d28e5ba59e57e7
Victor Yacovlev <v.yacovlev@gmail.com>
2015-10-27 16:45:38 +0300

    Fixed #kumir2-1968

--- share/kumir2/coregui/startpage/russian/actions.js
+++ share/kumir2/coregui/startpage/russian/actions.js
@@ -1,29 +1,44 @@
-function resizeEventHandler()
-{    
-    var w = window.innerWidth;
-    var h = window.innerHeight;
-    var doc = document.getElementById("central_column");
-    var footer = document.getElementById("footer");
-    var minW = 900;
-    if (w < minW) {
-        doc.style.display = "none";
-    }
-    else {
-        doc.style.display = "inline-block";
-    }
-    var minH = 350;
-    if (h < minH) {
-        footer.style.display = "none";
-    }
-    else {
-        footer.style.display = "block"
-    }
-}
+//function resizeEventHandler()
+//{
+//     var w = window.innerWidth;
+//     var h = window.innerHeight;
+//     var doc = document.getElementById("central_column");
+//     var footer = document.getElementById("footer");
+//     var minW = 900;
+//     if (w < minW) {
+//         doc.style.display = "none";
+//     }
+//     else {
+//         doc.style.display = "inline-block";
+//     }
+//     var minH = 350;
+//     if (h < minH) {
+//         footer.style.display = "none";
+//     }
+//     else {
+//         footer.style.display = "block"
+//     }
+//}
 
 function loadRecentFiles()
 {
-    var recentFiles = mainWindow.recentFiles(false);
-    var recentFilesFP = mainWindow.recentFiles(true);
+    "use strict";
+    var recentFiles = [];
+    var recentFilesFP = [];
+    try {
+        var mw = mainWindow;
+        recentFiles = mw.recentFiles(false);
+        recentFilesFP = mw.recentFiles(true);
+    }
+    catch (e) {
+        // Debugging in Web Browser?
+        recentFiles = ["file1.kum", "file2.kum", "file3.kum",
+            "file4.kum", "file5.kum", "file6.kum",
+            "file7.kum", "file8.kum", "file9.kum",
+            "file10.kum", "file11.kum", "file12.kum"
+        ];
+        recentFilesFP = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12"];
+    }
     var block = document.getElementById("recent_list");
     block.innerHTML = "";    
     for (var i=0; i<recentFiles.length; i++) {
@@ -43,7 +58,16 @@ function loadRecentFiles()
 
 function loadHelpContents()
 {
-    var contents = gui.helpList();
+    "use strict";
+    var contents = [];
+    try {
+        var g = gui;
+        contents = g.helpList();
+    }
+    catch (e) {
+        // Not running from Kumir?
+        contents = ["Справка 1", "Справка 2", "Справка 3", "Справка 4", "Справка 5", "Справка 6", "Справка 7"];
+    }
     var block = document.getElementById("help_list");
     block.innerHTML = "";
     for (var i=0; i<contents.length; i++) {
@@ -63,8 +87,19 @@ function loadHelpContents()
 
 function loadCourseContents()
 {
-    var contents = gui.coursesList(false);
-    var paths = gui.coursesList(true);
+    "use strict";
+    var contents = [];
+    var paths = [];
+    try {
+        var g = gui;
+        contents = gui.coursesList(false);
+        paths = gui.coursesList(true);
+    }
+    catch (e) {
+        // Not running from Kumir?
+        contents = ["Практикум 1", "Практикум 2", "Практикум 3", "Практикум 4", "Практикум 5", "Практикум 6", "Практикум 7"];
+        paths = ["1", "2", "3", "4", "5", "6", "7"];
+    }
     var block = document.getElementById("courses_list");
     block.innerHTML = "";
     for (var i=0; i<contents.length; i++) {
@@ -84,7 +119,7 @@ function loadCourseContents()
 
 function init()
 {
-    resizeEventHandler();
+    //resizeEventHandler();
     loadRecentFiles();
     loadHelpContents();
     loadCourseContents();
