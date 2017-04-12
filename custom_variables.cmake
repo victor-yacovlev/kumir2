023ffda57d8debdcc6ab8bc17dbeabbfd5d8725d
Victor Yacovlev <v.yacovlev@gmail.com>
2017-04-12 17:42:01 +0300

    New CMake project layout initial commit

---
+++ custom_variables.cmake
@@ -0,0 +1,7 @@
+# Copy this file to custom_variables.cmake
+# and override everithyng to your needs
+
+set(USE_QT 5)
+set(CMAKE_BUILD_TYPE Debug)
+set(CMAKE_C_COMPILER /usr/bin/gcc-6)
+set(CMAKE_CXX_COMPILER /usr/bin/g++-6)
--- kumir2_common.cmake
+++ kumir2_common.cmake
@@ -16,3 +16,36 @@ macro(add_opt_subdirectory SUBDIR_NAME)
         endif()
     endif()
 endmacro(add_opt_subdirectory)
+
+if(NOT DEFINED USE_QT)
+    set(USE_QT 4)
+endif(NOT DEFINED USE_QT)
+
+if(NOT USE_QT GREATER 4)
+    message(WARNING "Support for Qt4 will be discontinued soon. Please upgrade to Qt5!")
+endif()
+
+if(CMAKE_BUILD_TYPE MATCHES Debug)
+    set(CMAKE_VERBOSE_MAKEFILE on)
+endif()
+
+set(CMAKE_MODULE_PATH ${CMAKE_MODULE_PATH} "${CMAKE_SOURCE_DIR}")
+
+include_directories(${CMAKE_SOURCE_DIR}/src/shared)
+include_directories(${CMAKE_SOURCE_DIR}/src)
+
+# Finds Qt modules by components name
+function(use_qt)
+    set(_QT_COMPONENTS )
+    foreach(_component ${ARGN})
+        if(${USE_QT} GREATER 4)
+            if(${_component} MATCHES "Gui")
+                list(APPEND _QT_COMPONENTS "Gui" "Widgets")
+            else()
+                list(APPEND _QT_COMPONENTS ${_component})
+            endif()
+        else()
+            list(APPEND _QT_COMPONENTS "Qt${_component}")
+        endif()
+    endforeach(_component)
+endfunction(use_qt)
